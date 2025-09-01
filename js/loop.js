1d3bc122ea53d14bb23da46123d4f068ad176ecf
Neha Singh <neha.singh@coderower.com>
2025-09-01 18:16:00 +0530

    Added javascript loop file

---
+++ js/loop.js
@@ -0,0 +1,21 @@
+
+// for(let i=1;i<=10;i++){
+//     console.log(i);
+// }
+
+// for(let i=1;i<=10;i++){
+//     console.log("hello For Loop")
+// }
+// for(let i=5;i>=0;i--){
+//     console.log(i);
+// }
+// for and if -else
+    // for (let i=1;i<=5;i++){
+    //     if(i==3)
+    //         continue;
+    //     console.log(i);
+    // }
+// Check 
+ let age=15;
+        let status1=(age>=18) ? 'I can vote': 'I can not vote';
+        console.log(status1);
